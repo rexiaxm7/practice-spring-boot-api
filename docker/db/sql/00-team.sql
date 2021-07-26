64175ce487d24de6d3299e99838f552f895317fd
NakagawaKazuya <nakagawa_kazuya@crossware.co.jp>
2021-07-26 11:10:04 +0900

    テーブル定義変更、APIのCROUD対応

--- docker/db/sql/00-team.sql
+++ docker/db/sql/00-team.sql
@@ -1,10 +1,13 @@
 create table team
 (
-    id serial not null
+    id               serial             not null
         constraint team_pk
             primary key,
-    name varchar(20) not null
+    name             varchar(20)        not null,
+    input_start_date integer default 20 not null,
+    alert_start_days integer default 25 not null
 );
 
-alter table team owner to test;
+alter table team
+    owner to test;
 
