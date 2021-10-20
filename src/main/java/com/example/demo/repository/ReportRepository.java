b306df95cd25fbe5091fdc2874ec0d861a4717da
B4129 <B4129@oic.jp>
2021-10-20 16:55:19 +0900

    fix: チームテーブルを紐づけ

--- src/main/java/com/example/demo/repository/ReportRepository.java
+++ src/main/java/com/example/demo/repository/ReportRepository.java
@@ -10,7 +10,7 @@ import java.util.List;
 public interface ReportRepository extends JpaRepository<Report, Integer> {
 
     // 対象のチームID、年月のレポートを取得する
-    @Query("SELECT r FROM Report r WHERE r.user_id IN (SELECT u.id FROM User u  WHERE u.team_id = :teamId) " +
+    @Query("SELECT r FROM Report r WHERE r.user_id IN (SELECT u.id FROM User u  WHERE u.team.id = :teamId) " +
             "AND r.year = :thisYear AND r.month =:thisMonth")
     List<Report> findByTeamId(@Param("teamId") int teamId,@Param("thisYear") int thisYear,@Param("thisMonth") int thisMonth);
 }
